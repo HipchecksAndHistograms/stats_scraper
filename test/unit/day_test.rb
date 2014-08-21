f4c3aa7774641e4a6e3472c97793ef30924102fb
Nick Evans <me@nicolasevans.org>
2014-08-21 00:13:48 -0400

    format array of games nicer

--- test/unit/day_test.rb
+++ test/unit/day_test.rb
@@ -11,9 +11,21 @@ class DayTest < Minitest::Test
 
   def test_day_with_games_returns_properly
     @day = StatsScraper::Day.new(Time.new(2014, 3, 1))
+    expected = [
+      "2013020902",
+      "2013020903",
+      "2013020904",
+      "2013020905",
+      "2013020906",
+      "2013020907",
+      "2013020908",
+      "2013020909",
+      "2013020910",
+      "2013020911"
+    ]
 
     VCR.use_cassette('test_day_with_games_returns_properly') do
-      assert_equal ["2013020902", "2013020903", "2013020904", "2013020905", "2013020906", "2013020907", "2013020908", "2013020909", "2013020910", "2013020911"], @day.games
+      assert_equal expected, @day.games
     end
   end
 end
