794b2c68cee57ee71c50276d6fa391739d31c372
Nick Evans <me@nicolasevans.org>
2014-08-25 00:25:59 -0400

    reduce date range to 2 days and add check for event count

--- test/unit/date_range_test.rb
+++ test/unit/date_range_test.rb
@@ -2,17 +2,18 @@ require 'test_helper'
 
 class DateRangeTest < Minitest::Test
   def setup
-    start_date, end_date = Date.new(2014, 1, 1), Date.new(2014, 1, 10)
+    start_date, end_date = Date.new(2014, 1, 1), Date.new(2014, 1, 2)
     @range = StatsScraper::DateRange.new(start_date, end_date)
   end
 
   def test_date_range_is_correct_length
-    assert_equal 10, @range.days.length
+    assert_equal 2, @range.days.length
   end
 
   def test_date_range_scrapes_days
     VCR.use_cassette('test_date_range') do
-      assert_equal 64, @range.days.map(&:games).flatten.count
+      assert_equal 12, @range.days.map(&:games).flatten.count
+      assert_equal 3173, @range.days.map(&:games).flatten.map(&:events).flatten.count
     end
   end
 end
