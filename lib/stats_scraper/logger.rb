65d21831b217b7888fbad386c534cd0edab3c0f2
Nick Evans <me@nicolasevans.org>
2014-09-30 18:41:10 -0400

    fix logger module

--- lib/stats_scraper/logger.rb
+++ lib/stats_scraper/logger.rb
@@ -3,11 +3,11 @@ require 'logger'
 module StatsScraper
   module Logger
     def self.logger
-      @logger ||= Logger.new(STDOUT)
+      @logger ||= ::Logger.new(STDOUT)
     end
 
     def self.log(progname, message)
-      self.logger.add(Logger::INFO, message, progname) unless StatsScraper.test?
+      self.logger.add(::Logger::INFO, message, progname) unless StatsScraper.test?
     end
   end
 end
